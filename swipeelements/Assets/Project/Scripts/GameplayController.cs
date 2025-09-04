7834234e389d59588997d424c517a76255eb2827
sqveezee <danila.shintar@gmail.com>
2025-09-04 20:48:03 +0200

    setup project

---
+++ swipeelements/Assets/Project/Scripts/GameplayController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
